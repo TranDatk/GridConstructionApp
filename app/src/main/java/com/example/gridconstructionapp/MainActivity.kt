29b4563d41775db9f82c8de4fc7df618a6b42117
TranDatk <hgooshvhd123@gmail.com>
2023-05-14 14:55:41 +0700

    Hoàn thành

--- app/src/main/java/com/example/gridconstructionapp/MainActivity.kt
+++ app/src/main/java/com/example/gridconstructionapp/MainActivity.kt
@@ -4,23 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gridconstructionapp.data.DataSource
-import com.example.gridconstructionapp.data.Datasource
 import com.example.gridconstructionapp.model.Topic
 import com.example.gridconstructionapp.ui.theme.GridConstructionAppTheme
 
@@ -28,18 +34,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GridConstructionAppTheme {
+            GridConstructionAppTheme() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    TopicGrid(
+                        modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                    )
                 }
             }
         }
     }
 }
+
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -50,8 +59,8 @@ fun TopicGrid(modifier: Modifier = Modifier) {
         horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
         modifier = modifier
     ) {
-        items(DataSource.topics) { topic ->
-            TopicCard(topic)
+        items(DataSource.topics){ topic ->
+            TopicCard(topic = topic)
         }
     }
 }
@@ -74,7 +83,7 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
             Column {
                 Text(
                     text = stringResource(id = topic.name),
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = MaterialTheme.typography.body1,
                     modifier = Modifier.padding(
                         start = dimensionResource(R.dimen.padding_medium),
                         top = dimensionResource(R.dimen.padding_medium),
@@ -91,7 +100,6 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
                     )
                     Text(
                         text = topic.availableCourses.toString(),
-                        style = MaterialTheme.typography.labelMedium,
                         modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_small))
                     )
                 }
@@ -103,7 +111,7 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun TopicPreview() {
-    CoursesTheme {
+    GridConstructionAppTheme() {
         val topic = Topic(R.string.photography, 321, R.drawable.photography)
         Column(
             modifier = Modifier.fillMaxSize(),
